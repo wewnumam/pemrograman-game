073ad4f21c1d40b3e7e21d82841bed0879446a1e
Ahmad Adillaumam <44931897+wewnumam@users.noreply.github.com>
2022-08-20 10:19:40 +0700

    Complete Roll a Ball Project

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
  Mata Kuliah Pemrograman Game Semester 3 Prodi Teknologi Permainan STMM MMTC Yogyakarta 2022
 
 - [Instalasi dan Aktivasi Unity](./instalasi-dan-aktivasi-unity/)
+- [Roll a Ball](./roll-a-ball/)
--- roll-a-ball/Assets/Scripts/PlayerController.cs
+++ roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -1,25 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// Include the namespace required to use Unity UI and Input System
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    // Create public variables for player speed, and for the Text UI game objects
     public float speed = 0;
+    public TextMeshProUGUI countText;
+	public GameObject winTextObject;
     
     private Rigidbody rb;
-
+    private int count;
     private float movementX;
     private float movementY;
 
-    // Start is called before the first frame update
+    // At the start of the game..
     void Start()
     {
+        // Assign the Rigidbody component to our private rb variable
         rb = GetComponent<Rigidbody>();
+
+        // Set the count to zero 
+		count = 0;
+
+		SetCountText();
+
+        // Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
     {
+        // Create a Vector3 variable, and assign X and Z to feature the horizontal and vert
         Vector2 movementVector = movementValue.Get<Vector2>();
 
         movementX = movementVector.x;
@@ -28,8 +43,35 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        // Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         
         rb.AddForce(movement * speed);
     }
+
+    void OnTriggerEnter(Collider other) 
+	{
+		// ..and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+
+			// Add one to the score variable 'count'
+			count = count + 1;
+
+			// Run the 'SetCountText()' function (see below)
+			SetCountText();
+		}
+	}
+
+    void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 12) 
+		{
+            // Set the text value of your 'winText'
+            winTextObject.SetActive(true);
+		}
+	}
 }
